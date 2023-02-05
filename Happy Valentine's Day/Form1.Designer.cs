fa37af37c385864fda0f3bcc63de39e8922ff929
Alcaeus Andrea Conner <001031349@tarleton.edu>
2023-02-05 13:37:12 -0600

    Add project files.

---
+++ Happy Valentine's Day/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿
+namespace Happy_Valentine_s_Day
+{
+    partial class vDay
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(vDay));
+            this.confessionLabel = new System.Windows.Forms.Label();
+            this.questionButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // confessionLabel
+            // 
+            this.confessionLabel.AutoSize = true;
+            this.confessionLabel.Font = new System.Drawing.Font("Microsoft Uighur", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.confessionLabel.ForeColor = System.Drawing.Color.Firebrick;
+            this.confessionLabel.Location = new System.Drawing.Point(95, 23);
+            this.confessionLabel.Name = "confessionLabel";
+            this.confessionLabel.Size = new System.Drawing.Size(122, 26);
+            this.confessionLabel.TabIndex = 0;
+            this.confessionLabel.Text = "I have a confession...";
+            // 
+            // questionButton
+            // 
+            this.questionButton.Font = new System.Drawing.Font("Microsoft Tai Le", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.questionButton.ForeColor = System.Drawing.Color.Firebrick;
+            this.questionButton.Location = new System.Drawing.Point(115, 52);
+            this.questionButton.Name = "questionButton";
+            this.questionButton.Size = new System.Drawing.Size(75, 23);
+            this.questionButton.TabIndex = 1;
+            this.questionButton.Text = "What is it?";
+            this.questionButton.UseVisualStyleBackColor = true;
+            this.questionButton.Click += new System.EventHandler(this.questionButton_Click);
+            // 
+            // vDay
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Pink;
+            this.ClientSize = new System.Drawing.Size(295, 99);
+            this.Controls.Add(this.questionButton);
+            this.Controls.Add(this.confessionLabel);
+            this.Font = new System.Drawing.Font("Microsoft Tai Le", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.LightSalmon;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "vDay";
+            this.Text = "My message to you...";
+            this.TransparencyKey = System.Drawing.Color.White;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label confessionLabel;
+        private System.Windows.Forms.Button questionButton;
+    }
+}
+
---
+++ Happy Valentine's Day/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Happy_Valentine_s_Day
+{
+    public partial class vDay : Form
+    {
+        public vDay()
+        {
+            InitializeComponent();
+        }
+
+        private void questionButton_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("I like you, will you be valentine now and forever?");
+            DialogResult dialogResult = MessageBox.Show("Will you reciprocate those feelings back to me?", "It would be nice if you accept it...", MessageBoxButtons.YesNo);
+            if (dialogResult == DialogResult.Yes)
+            {
+                ;
+                MessageBox.Show("Oh my gosh, you feel the same. Let's be lovers <3");
+                this.Close();
+            }
+
+            else if (dialogResult == DialogResult.No)
+            {
+                ;
+                MessageBox.Show("It is okay, I am used to it.");
+                this.Close();
+            }
+        }
+    }
+}
